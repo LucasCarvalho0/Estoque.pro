77e7696610c0ff2290e6960bbdefa48840ad724e
Your Name <you@example.com>
2026-05-20 09:33:45 -0300

    chore: remove scratch folder to fix vercel build

--- scratch/check_fields.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  console.log("Fields exists?", !!(prisma.product as any).fields);
-  process.exit(0);
-}
-main();
